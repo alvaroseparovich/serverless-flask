4842a26ea10d677fe2aa101b173add96fd4e17a7
Alvaro Separovich <alvasepro@gmail.com>
2019-09-26 09:41:38 -0300

    Revert "Verify  if order has no item"
    This reverts commit 042807852e42614924bd2d3c90c8da24798dbaed.

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['POST', 'GET'])
+@app.route("/")
 def main():
-    
-    data = json.loads(request.get_data())
-    print("Id processed:  ----- " + str(data['id']))
-    return ""
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
